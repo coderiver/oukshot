9c7dcae7269bcbe3f5719010a6847dae2d4bd199
hraboviyvadim <hraboviyvadim@gmail.com>
2014-02-03 16:35:58 +0200

    inner pages

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $("#single_1,#single_2").fancybox({
+    	openEffect	: 'elastic',
+    	closeEffect	: 'elastic',
+
+    	helpers : {
+    		title : {
+    			type : 'inside'
+    		}
+    	}
+    });
+    
+
+});
