b114bcc3fbe9651d92575b8c93eccc4ddc375bdf
hraboviyvadim <hraboviyvadim@gmail.com>
2014-02-04 17:18:44 +0200

    upd

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
-    $("#single_1,#single_2").fancybox({
-    	openEffect	: 'elastic',
-    	closeEffect	: 'elastic',
+    // $("#single_1,#single_2").fancybox({
+    // 	openEffect	: 'elastic',
+    // 	closeEffect	: 'elastic',
 
-    	helpers : {
-    		title : {
-    			type : 'inside'
-    		}
-    	}
-    });
-    
+    // 	helpers : {
+    // 		title : {
+    // 			type : 'inside'
+    // 		}
+    // 	}
+    // });
+    $('.parallax-layer').parallax({
+            mouseport: $(".header"),
+            yparallax: false,
+         	
+    },
+    {xparallax: '0'},
+    {xparallax: '110px'},
+    {xparallax: '200px'},
+    {xparallax: '100px'}
+    );
 
 });
